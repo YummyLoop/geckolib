9282b7e44ae368fb897814f4b2b08bba8d1aeb32
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2021-02-15 23:04:17 +0000

    Fix armor on forge

--- forge/src/main/java/software/bernie/geckolib3/forge/item/GeoArmorItem.java
+++ forge/src/main/java/software/bernie/geckolib3/forge/item/GeoArmorItem.java
@@ -1,38 +1,39 @@
-package software.bernie.geckolib3.item;
+package software.bernie.geckolib3.forge.item;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import software.bernie.geckolib3.ArmorProvider;
 import software.bernie.geckolib3.renderer.geo.GeoArmorRenderer;
 
-public abstract class GeoArmorItem extends ArmorItem implements ArmorProvider {
+public abstract class GeoArmorItem extends ArmorItem {
 	public GeoArmorItem(ArmorMaterial materialIn, EquipmentSlot slot, Settings builder) {
 		super(materialIn, slot, builder);
 	}
 
-	@Environment(EnvType.CLIENT) //TODO: Check if this works in forge
-	public BipedEntityModel<LivingEntity> getArmorModel(LivingEntity entityLiving, ItemStack itemStack,
-			EquipmentSlot armorSlot, BipedEntityModel<LivingEntity> _default) {
+	@OnlyIn(Dist.CLIENT)
+	@Override
+	public <A extends BipedEntityModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack,
+			EquipmentSlot armorSlot, A _default) {
 		Class<? extends ArmorItem> clazz = this.getClass();
 		GeoArmorRenderer renderer = GeoArmorRenderer.getRenderer(clazz);
 		renderer.applyEntityStats(_default).applySlot(armorSlot);
 		renderer.setCurrentItem(entityLiving, itemStack, armorSlot);
-		return renderer;
+		return (A) renderer;
 	}
 
 	@Override
-	public Identifier getArmorTexture(LivingEntity entity, ItemStack stack, EquipmentSlot slot,
-			Identifier defaultTexture) {
+	public final String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
 		Class<? extends ArmorItem> clazz = this.getClass();
 		GeoArmorRenderer renderer = GeoArmorRenderer.getRenderer(clazz);
-		return renderer.getTextureLocation((ArmorItem) stack.getItem());
+		return renderer.getTextureLocation((ArmorItem) stack.getItem()).toString();
 	}
 
 }
